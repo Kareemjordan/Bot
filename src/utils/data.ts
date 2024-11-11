8257688e71f7a9cc8c36b37e760ec24d142c7351
Matt Richards <me@mttrchrds.com>
2024-11-11 09:52:15 +0000

    Tidies up code.

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -34,11 +34,6 @@ export const scrapeMeta = async (agent: AtpAgent, uri: string) => {
     // Scrapes meta image and uploads it to Bluesky CDN
     if (title && description) {
       const image = $('meta[property="og:image"]').attr("content");
-      // const uri = 'https://www.independent.co.uk/sport/football/liverpool-injuries-team-news-arne-slot-tsimikas-robertson-b2643620.html'
-      // const title = 'Slot gives verdict on key battle for position at Liverpool'
-      // const description = 'The Reds boss also has a decision to make in attack after Luis Diaz scored three'
-      // const image = 'https://static.independent.co.uk/2024/11/08/09/newFile-2.jpg?quality=75&width=1200&auto=webp'
-
       if (image) {
         const remoteImage = await axios.get(image, {
           responseType: "arraybuffer",
